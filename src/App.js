c76ae132de7d2f8776754e0ccdc01c0ad9347745
Noel Bryant <noelsobryant@gmail.com>
2023-10-16 16:00:36 +0300

    remove dragons component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navigation from './components/header/NavBar';
 import Rockets from './components/rockets/Rockets';
 import Missions from './components/missions/Missions';
-import Dragons from './components/dragons/Dragons';
 import MyProfile from './components/myprofile/MyProfile';
 import './App.css';
 
@@ -16,7 +15,6 @@ function App() {
         <Switch>
           <Route path="/rockets" component={Rockets} />
           <Route path="/missions" component={Missions} />
-          <Route path="/dragons" component={Dragons} />
           <Route path="/my-profile" component={MyProfile} />
         </Switch>
       </div>
