c45c2da2001ce0851b29b752aebb1849f3931983
Noel Bryant <noelsobryant@gmail.com>
2023-10-17 05:01:42 +0300

    access store state,dispatch fetchRockets action and render rockets

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../../redux/features/rockets/rocketsSlice';
 
-const Rockets = () => (
-  <div>
-    <h2>Rockets Page</h2>
-    {/*  */}
-  </div>
-);
+const Rockets = () => {
+  const rocketsState = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  return (
+    <section className="rockets-container">
+      <ul className="rocket-info-container">
+        {rocketsState.rockets.map((rocket) => (
+          <li key={rocket.id}>
+            {rocket.flickr_images.length > 0 && (
+              <img
+                src={rocket.flickr_images[0]}
+                alt={`Rocket ${rocket.name} Imge`}
+                className="rocket-image"
+              />
+            )}
+            <div className="rocket-info">
+              <h2>{rocket.name}</h2>
+              <p>{rocket.description}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 export default Rockets;
