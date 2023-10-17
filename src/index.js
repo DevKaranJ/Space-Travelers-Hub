08a92f5da53d74cf0cf352a7b5163b409c5babad
Karan Joshi <Karan190213@gmail.com>
2023-10-17 18:24:49 +0530

    Update index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
-import store from './redux/store';
-
-import './index.css';
+import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 import App from './App';
 import './index.css';
 
