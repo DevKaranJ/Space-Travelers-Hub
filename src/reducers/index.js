3b4b180b027d21a65505a412edf328575591f2cc
Karan Joshi <Karan190213@gmail.com>
2023-10-20 18:31:51 +0530

    Conflict Error FIx
    Created a single store.js file for all reducers

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import missionsReducer from './missions/missionsReducer';
-
-const rootReducer = combineReducers({
-  missions: missionsReducer,
-});
-
-export default rootReducer;
