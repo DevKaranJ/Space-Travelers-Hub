edefb7e7d598e8b65943cfe28934610506a03331
Karan Joshi <Karan190213@gmail.com>
2023-10-20 19:12:53 +0530

    My profile section update

--- src/components/myprofile/MyProfile.js
+++ src/components/myprofile/MyProfile.js
@@ -1,21 +1,56 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import './MyProfile.css';
 
 const MyProfile = () => {
   const rocketsState = useSelector((state) => state.rockets.rockets);
+  const missionsState = useSelector((state) => state.missions.missions);
 
   const reservedRockets = rocketsState.filter((rocket) => rocket.reserved);
+  const reservedMissions = missionsState.filter((mission) => mission.reserved);
 
   return (
-    <section className="my-rockets">
-      <h2>My Rockets</h2>
-      <ul>
-        {reservedRockets.map((rocket) => (
-          <li key={rocket.id}>
-            <p>{rocket.name}</p>
-          </li>
-        ))}
-      </ul>
+    <section className="my-profile">
+      <div className="rocket-profileTable">
+        {reservedRockets.length > 0 ? (
+          <table className="profile-table">
+            <thead>
+              <tr>
+                <th>Rockets</th>
+              </tr>
+            </thead>
+            <tbody>
+              {reservedRockets.map((rocket) => (
+                <tr key={rocket.id}>
+                  <td>{rocket.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>No rockets reserved.</p>
+        )}
+      </div>
+      <div className="mission-profileTable">
+        {reservedMissions.length > 0 ? (
+          <table className="profile-table">
+            <thead>
+              <tr>
+                <th>Mission Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {reservedMissions.map((mission) => (
+                <tr key={mission.mission_id}>
+                  <td>{mission.mission_name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>No missions joined.</p>
+        )}
+      </div>
     </section>
   );
 };
